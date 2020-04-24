55bd3e4e037e101e8c3dc6a8022e31e05e64d276
pmoorey <petermoorey@gmail.com>
2020-04-24 18:45:28 +0000

    Added event emitter for notifications

--- bgp-visibility/src/app/config-edit/config-edit.component.ts
+++ bgp-visibility/src/app/config-edit/config-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-config-edit',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./config-edit.component.css']
 })
 export class ConfigEditComponent implements OnInit {
+  @Output() eventCreated = new EventEmitter<string>();
+  prefixList = [];
+  prefix = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClickAddPrefix() {
+    this.prefixList.push(this.prefix);
+    this.eventCreated.emit('Created ' + this.prefix);
+    console.log('output:' + this.prefix);
   }
 
 }
--- bgp-visibility/src/app/nav/nav.component.ts
+++ bgp-visibility/src/app/nav/nav.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { Settings } from '../settings/settings.model'
+import { Settings } from '../settings/settings.model';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +10,7 @@ import { Settings } from '../settings/settings.model'
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
-
+  events = [];
   settings = new Settings('Peter', 'Moorey', 'petermoorey@gmail.com');
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
@@ -21,4 +21,8 @@ export class NavComponent {
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
+  processEvents(event: string){
+    this.events.push(event);
+    console.log('recieved' + event);
+  }
 }
