5b5740ecdbae1099eb90074ac3434343cd0d71af
pmoorey <petermoorey@gmail.com>
2020-05-21 15:05:56 +0000

    Changed process for selecting prefixes to be monitored

--- bgp-visibility/src/app/app.module.ts
+++ bgp-visibility/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatBadgeModule } from '@angular/material/badge';
-
+import { MatTable } from '@angular/material/table';
 import { AppComponent } from './app.component';
 import { ConfigListComponent } from './config-list/config-list.component';
-import { ConfigEditComponent } from './config-edit/config-edit.component';
 import { EventDashboardComponent } from './event-dashboard/event-dashboard.component';
 import { NavComponent } from './nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -37,7 +36,6 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [
     AppComponent,
     ConfigListComponent,
-    ConfigEditComponent,
     EventDashboardComponent,
     NavComponent,
     SettingsComponent,
@@ -57,6 +55,7 @@ import { AppRoutingModule } from './app-routing.module';
     MatSidenavModule,
     MatListModule,
     MatCardModule,
+    MatTable,
     MatExpansionModule,
     FlexLayoutModule,
     MatProgressSpinnerModule,
--- bgp-visibility/src/app/event-dashboard/event-dashboard.component.ts
+++ bgp-visibility/src/app/event-dashboard/event-dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BgpDataService } from '../bgp-data.service';
-
+import { DatabaseService } from '../database.service';
+import { Notification } from '../notification.model';
+import { Prefix } from '../prefix.model';
 
 @Component({
   selector: 'app-event-dashboard',
@@ -17,11 +19,18 @@ export class EventDashboardComponent implements OnInit {
   Status = '';
   REST_API_QUERY = '';
   panelOpenState = false;
+  username = 'pmoorey';
 
-
-  constructor(public dataService: BgpDataService) {}
+  constructor(public bgpDataService: BgpDataService, public dataService: DatabaseService) {}
 
   ngOnInit() {
+  }
+  onClickAddPrefix(prefixData: string) {
+    // create prefix and notify
+    const notification = new Notification(null, 'Added ' + prefixData, 'info', this.username, false);
+    const prefix = new Prefix(null, prefixData, this.username);
+    this.dataService.createPrefix(prefix);
+    this.dataService.createNotification(notification);
   }
 
   onClickAddASN() {
@@ -66,24 +75,24 @@ export class EventDashboardComponent implements OnInit {
     }
 
     // get overview
-    this.dataService.getAsOverview(this.ASN).subscribe((data: ResponseOverview) =>  {
+    this.bgpDataService.getAsOverview(this.ASN).subscribe((data: ResponseOverview) =>  {
       this.overview = data.data;
       console.log(this.overview.holder);
     });
 
     // get prefixes
-    this.dataService.getAsPrefixes(this.ASN).subscribe((data: PrefixResponse) =>  {
+    this.bgpDataService.getAsPrefixes(this.ASN).subscribe((data: PrefixResponse) =>  {
       this.prefixes = data.data.prefixes;
     });
 
     // get peers
-    this.dataService.getAsNeighbors(this.ASN).subscribe((data: PeersResponse) =>  {
+    this.bgpDataService.getAsNeighbors(this.ASN).subscribe((data: PeersResponse) =>  {
       this.peers = data.data.neighbours;
     });
 
     // this.peers.forEach(function(value) {
     //   console.log('processing' + value.asn);
-    //   this.dataService.getAsOverview('72').subscribe((data: any[data]) =>  {
+    //   this.bgpDataService.getAsOverview('72').subscribe((data: any[data]) =>  {
     //     console.log('made api call');
     //     // console.log(data.data);
     //     // this.peersNames.push(data.data.holder);
